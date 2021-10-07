7bfa2296e540c308c53c5ac1769f6637030fd570
Hamilton97 <ryangilberthamilon@gmail.com>
2021-10-07 11:14:33 -0400

    added bats to build output rasters from csv inputs

---
+++ ryan/python/arcpy/build-raster/build-components.bat
@@ -0,0 +1,1 @@
+c:\Progra~1\ArcGIS\Pro\bin\Python\scripts\propy.bat point_to_raster.py
---
+++ ryan/python/arcpy/build-raster/run.bat
@@ -0,0 +1,3 @@
+c:\Progra~1\ArcGIS\Pro\bin\Python\scripts\propy.bat build_shapefile.py
+c:\Progra~1\ArcGIS\Pro\bin\Python\scripts\propy.bat point_to_raster.py
+
