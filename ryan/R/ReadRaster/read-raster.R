ae8f7a626a7cbc930840018a11682c1c79a1e107
Ryan Hamilton <ryangilberthamilton@gmail.com>
2021-11-22 11:28:33 -0500

    added read from raster

---
+++ ryan/R/ReadRaster/read-raster.R
@@ -0,0 +1,9 @@
+
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(raster)
+library(rgdal)
+
+# read in file names of csvs
+files <- "/home/rhamilton/projects/nwrc-bc-cloud-dev/ryan/R/ReadRaster/ee_stack_2019_side_1.tif"
