17d626465dfc97fa32a9032bd08bee752504312e
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-02-22 14:51:36 -0500

    updated: preprocess-training-data.R

--- ken/002-read-geojsons/code/preprocess-training-data.R
+++ ken/002-read-geojsons/code/preprocess-training-data.R
@@ -13,30 +13,38 @@ preprocess.training.data <- function(
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     colnames(DF.output) <- tolower(colnames(DF.output));
-    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^class$", replacement = "land.cover");
+    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^class$",   replacement = "land_cover");
     colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^point_x$", replacement = "longitude");
     colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^point_y$", replacement =  "latitude");
-    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^vv$", replacement = "VV");
-    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^vh$", replacement = "VH");
+    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^vv$",      replacement = "VV");
+    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^vh$",      replacement = "VH");
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     DF.output[,'date']       <- as.Date(DF.output[,'date']);
-    DF.output[,'land.cover'] <- factor(DF.output[,'land.cover']);
+    DF.output[,'land_cover'] <- factor(DF.output[,'land_cover']);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.output <- DF.output[order(DF.output$latitude,DF.output$longitude,DF.output$date),];
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.output[,'lat_lon'] <- apply(
+        X      = DF.output[,c('latitude','longitude')],
+        MARGIN = 1,
+        FUN    = function(x) {return(paste(x,collapse="_"))}
+        );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     colnames.to.retain <- c(
         'latitude',
         'longitude',
-        'land.cover',
+        'lat_lon',
+        'land_cover',
         'date',
         'VV',
         'VH'
         );
 
     DF.output <- DF.output[,colnames.to.retain];
-
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    DF.output <- DF.output[order(DF.output$latitude,DF.output$longitude,DF.output$date),];
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     cat(paste0("\n# ",thisFunctionName,"() exits."));
