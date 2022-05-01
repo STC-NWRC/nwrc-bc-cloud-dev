a334ae0ef2687bed04d7671bf287c63c41482eef
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-05-01 12:29:23 -0400

    added (data download scripts and log files): ken/000-data/001-bay-of-quinte/

---
+++ ken/000-data/001-bay-of-quinte/training-data-geojson/get-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROJECT_ID=fpca-336015
+LOCATION=us-central1
+ZONE=us-central1-a
+EXTERNAL_BUCKET=gs://fpca-bay-of-quinte-test
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+# set the active project
+echo
+echo setting active project to: ${PROJECT_ID}
+gcloud config set project ${PROJECT_ID}
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### list contents of a bucket folder
+gsutil ls ${EXTERNAL_BUCKET}/*
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### copying contents of TrainingData_Geojson
+gsutil cp -r ${EXTERNAL_BUCKET}/TrainingData_Geojson/* .
+
